16796b63bf8c5632f86c49078829b1dd8e313831
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2024-03-16 19:08:35 -0300

    feat: define our initial tokens

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DaviidSantos/writing-an-interpreter-in-go
+
+go 1.21.3
---
+++ src/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// OPERATORS
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	DEF      = "DEF"
+)
+
+type TokenType string
+
+type token struct {
+	Type    TokenType
+	Literal string
+}
